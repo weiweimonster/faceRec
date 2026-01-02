07f225a6171de144772dea79b46f8cd05ec4834a
weiweimonster <weiweimonster1130@gmail.com>
2026-01-01 19:11:38 -0800

    Convert print to logger

--- main.py
+++ main.py
@@ -7,6 +7,7 @@ from src.ingestion.processor import FeatureExtractor
 from src.ingestion.format_handler import ensure_display_version
 from src.clustering.service import ClusteringService
 from src.utils import calculate_image_hash
+from src.util.logger import logger
 
 RAW_PHOTOS_DIR = "./photos"
 CACHE_DIR = "./photos/cache"
@@ -19,7 +20,7 @@ def run_ingestion() -> None:
     and saves the embeddings in the database.
     """
 
-    print("🚀 Starting Ingestion Pipeline...")
+    logger.info("🚀 Starting Ingestion Pipeline...")
 
     # Initialize Tools
     # We initialize here to avoid loading heavy models if we are only running clustering
@@ -33,19 +34,19 @@ def run_ingestion() -> None:
             if f.lower().endswith(('.jpg', '.jpeg', '.png', '.heic', '.webp')):
                 all_files.append(os.path.join(dp, f))
 
-    print(f"📸 Found {len(all_files)} photos.")
+    logger.info(f"📸 Found {len(all_files)} photos.")
 
     # Process loop
     for raw_path in tqdm(all_files, desc="Ingesting"):
         try:
             img_hash = calculate_image_hash(raw_path)
         except Exception as e:
-            print(f"Error calculating hash for {raw_path}: {e}. Skipping.")
+            logger.error(f"Error calculating hash for {raw_path}: {e}. Skipping.")
             continue
         try:
             # Optimization: Skip if already in DB
             if db.photo_exists(img_hash):
-                print(f"Skipping duplicate: {raw_path}")
+                logger.warning(f"Skipping duplicate: {raw_path}")
                 continue
 
             # Convert/Validate Image (Handle HEIC)
@@ -58,19 +59,19 @@ def run_ingestion() -> None:
                 # Save to DB
                 db.save_result(result, raw_path, display_path, img_hash)
             else:
-                print(f"⚠️  Skipping invalid file: {raw_path}")
+                logger.warning(f"⚠️  Skipping invalid file: {raw_path}")
         except Exception as e:
-            print(f"❌ Error processing {raw_path}: {e}")
+            logger.error(f"❌ Error processing {raw_path}: {e}")
 
     db.close()
-    print("✅ Ingestion Complete.")
+    logger.info("✅ Ingestion Complete.")
 
 def run_clustering() -> None:
     """
     Loads all unclustered faces from the database, groups them by identity,
     and updates the database records
     """
-    print("🧠 Starting Clustering Pipeline...")
+    logger.info("🧠 Starting Clustering Pipeline...")
 
     # We don't need the FeatureExtractor here (saves RAM)
     db = DatabaseManager(sql_path=DB_SQL_PATH, chroma_path=DB_CHROMA_PATH)
